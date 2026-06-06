9897cef3d6a67f31fc5e3be14e928e962dbb23ff
SRychok <srychok@insat.org.ua>
2026-06-06 15:35:14 +0300

    маніфест все щн правки

--- v1/vite.config.ts
+++ v1/vite.config.ts
@@ -39,6 +39,12 @@ export default defineConfig(() => {
             },
             {
               src: "icon-512.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "icon-512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "maskable",
